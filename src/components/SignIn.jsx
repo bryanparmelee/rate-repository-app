daf4a0faf28ef3e43efa3d603fe5e7df24d16543
bryanparmelee <bryan.parmelee@gmail.com>
2024-02-25 19:36:52 -0500

    Exercise 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,54 @@
+import { Pressable, View, StyleSheet } from "react-native";
+import { Formik } from "formik";
+
+import FormikTextInput from "./FormikTextInput";
 import Text from "./Text";
 
+import theme from "../theme";
+
+const SignInForm = ({ onSubmit }) => {
+  const styles = StyleSheet.create({
+    container: {
+      display: "flex",
+      gap: 8,
+      padding: 8,
+    },
+    submitButton: {
+      padding: 4,
+      backgroundColor: theme.colors.primary,
+      borderRadius: 5,
+    },
+  });
+  return (
+    <View style={styles.container}>
+      <Text fontWeight={"bold"}>Sign-in</Text>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput
+        name="password"
+        placeholder="Password"
+        secureTextEntry={true}
+      />
+      <Pressable style={styles.submitButton} onPress={onSubmit}>
+        <Text color={"white"}>Submit</Text>
+      </Pressable>
+    </View>
+  );
+};
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,23 @@
+import { TextInput as NativeTextInput, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  input: {
+    width: "full",
+    height: 30,
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 4,
+  },
+  errorText: {
+    marginTop: 5,
+    color: "red",
+  },
+});
+
+const TextInput = ({ error, ...props }) => {
+  const textInputStyle = error ? styles.errorText : styles.input;
+
+  return <NativeTextInput style={textInputStyle} {...props} />;
+};
+
+export default TextInput;
